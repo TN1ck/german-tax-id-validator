7a9714bb7d5efd678b36ded0b923e304f6c1443d
Tom Nick <tomwanick@gmail.com>
2024-05-09 22:15:34 +0200

    chore: improve tests, add github action

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it } from 'vitest'
-import {validate} from '../index'
+import taxValidator from '../index'
 
 const TEST_DATA = {
     valid_id_2015: '12345678911',
@@ -15,39 +15,39 @@ const TEST_DATA = {
 
 describe('#validate', function() {
     it('correctly validates a valid tax-id', function() {
-        expect(validate(TEST_DATA.valid_id_2015)).toBe(true);
+        expect(taxValidator.validate(TEST_DATA.valid_id_2015)).toBe(true);
     });
     it('correctly validates a valid 2016-tax-id', function() {
-        expect(validate(TEST_DATA.valid_id_2016)).toBe(true);
+        expect(taxValidator.validate(TEST_DATA.valid_id_2016)).toBe(true);
     });
     it('correctly invalidate a valid 2016-tax-id if the flag is set to true', function() {
-        expect(validate(TEST_DATA.valid_id_2016, false, true)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.valid_id_2016, false, true)).toBe(false);
     });
     it('correctly validate a valid 2016-tax-id if the flag is set to false', function() {
-        expect(validate(TEST_DATA.valid_id_2016, false, false)).toBe(true);
+        expect(taxValidator.validate(TEST_DATA.valid_id_2016, false, false)).toBe(true);
     });
     it('correctly invalidate a valid 2015-tax-id if the flag is set', function() {
-        expect(validate(TEST_DATA.valid_id_2015, true, false)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.valid_id_2015, true, false)).toBe(false);
     });
     it('correctly invalidate a tax-id with a wrong checksum', function () {
-        expect(validate(TEST_DATA.invalid_wrong_checksum)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.invalid_wrong_checksum)).toBe(false);
     });
     it('correctly invalidate a tax-id with with not enough unique numbers', function () {
-        expect(validate(TEST_DATA.invalid_not_enough_unique)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.invalid_not_enough_unique)).toBe(false);
     });
     it('correctly invalidates a tax-id that is too long', function() {
-        expect(validate(TEST_DATA.invalid_id_too_long)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.invalid_id_too_long)).toBe(false);
     });
     it('correctly invalidates a tax-id that is too short', function() {
-        expect(validate(TEST_DATA.invalid_id_too_short)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.invalid_id_too_short)).toBe(false);
     });
     it('correctly invalidates a tax-id that has no leading zero', function() {
-        expect(validate(TEST_DATA.invalid_id_no_leading_zero)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.invalid_id_leading_zero)).toBe(false);
     });
     it('does not crash with invalid input', function() {
-        expect(validate(TEST_DATA.invalid_input_1)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.invalid_input_1 as any)).toBe(false);
     });
     it('does not crash with invalid input', function() {
-        expect(validate(TEST_DATA.invalid_input_2)).toBe(false);
+        expect(taxValidator.validate(TEST_DATA.invalid_input_2 as any)).toBe(false);
     });
 });
